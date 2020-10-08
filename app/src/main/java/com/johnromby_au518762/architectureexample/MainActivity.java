9e70cf64ac7a60b6239e4082af9927e1d301d9d5
John Romby <romby@outlook.com>
2020-10-08 12:17:20 +0200

    Added ViewModel to MainActivity

--- app/src/main/java/com/johnromby_au518762/architectureexample/MainActivity.java
+++ app/src/main/java/com/johnromby_au518762/architectureexample/MainActivity.java
@@ -1,14 +1,36 @@
 package com.johnromby_au518762.architectureexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class); // ViewModelProviders is deprecated.
+
+        // This is the new way of doing it I believe
+        noteViewModel = new ViewModelProvider(this,
+                ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // Update RecyclerView...
+
+                // TODO Temporary Toast message, can be removed.
+                Toast.makeText(MainActivity.this, "Debug: onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
